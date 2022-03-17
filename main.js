95c78e0d96b9011eba32ac97b1e661a907db53f8
Mflavien01 <72614724+Mflavien01@users.noreply.github.com>
2022-03-17 22:25:31 +0100

    jsp

--- main.js
+++ main.js
@@ -153,6 +153,7 @@ jQuery(document).ready(function($) {
 	$('#time-Vac').val(heurVac);
 	if ("geolocation" in navigator) {
 		navigator.geolocation.getCurrentPosition(function(position) {
+			console.log(position.coords.longitude, position.coords.latitude)
 			userLongitude = position.coords.longitude;
 			userLatitude = position.coords.latitude;
 			findCity(userLongitude, userLatitude)
