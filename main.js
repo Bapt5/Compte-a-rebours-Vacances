e61f9d4948c35aef7424d7462fee61a07aa9c769
Mflavien01 <72614724+Mflavien01@users.noreply.github.com>
2022-03-18 18:34:27 +0100

    jsp

--- main.js
+++ main.js
@@ -30,15 +30,16 @@ function findCity(userLongitude, userLatitude) {
 	// console.log("3: ", typeof longitude, longitude, typeof latitude, latitude);
 	for (var i = 0; i < 36208; i++) {
 		// console.log("4: ", typeof longitude, longitude, typeof latitude, latitude);
-		findDistance(longitude, latitude, donnee[i].longitude, donnee[i].latitude);
-		if (city.distance > distance || Object.keys(city).length === 0) {
-			city = {}
-			city.departement = donnee[i].departement;
-			city.ville = donnee[i].ville;
-			city.longitude = donnee[i].longitude;
-			city.latitude = donnee[i].latitude;
-			city.distance = distance;
-		}
+		console.log(donnee[i].longitude);
+		// findDistance(longitude, latitude, donnee[i].longitude, donnee[i].latitude);
+		// if (city.distance > distance || Object.keys(city).length === 0) {
+		// 	city = {}
+		// 	city.departement = donnee[i].departement;
+		// 	city.ville = donnee[i].ville;
+		// 	city.longitude = donnee[i].longitude;
+		// 	city.latitude = donnee[i].latitude;
+		// 	city.distance = distance;
+		// }
 	}
 	console.log(city);
 }
