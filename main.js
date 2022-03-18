bba47048994e4d9c73bca87332e71fe161625560
Mflavien01 <72614724+Mflavien01@users.noreply.github.com>
2022-03-18 18:23:12 +0100

    jsp

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ function findCity(userLongitude, userLatitude) {
 	for (var j = 0; j < donnee.length; j++) {
 		console.log('ok');
 		console.log("4: ", typeof longitude, longitude, typeof latitude, latitude);
-		findDistance(longitude, latitude, donnee[j].longitude, donnee[j].latitude);
+		// findDistance(longitude, latitude, donnee[j].longitude, donnee[j].latitude);
 		// if (city.distance > distance || Object.keys(city).length === 0) {
 		// 	city = {}
 		// 	city.departement = donnee[j].departement;
