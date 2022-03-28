ef12760c73a5cab3f0c90a2ae1ef95edbf218a53
Flavien Mathieu <72614724+Mflavien01@users.noreply.github.com>
2022-03-28 10:51:56 +0200

    jsp

--- main.js
+++ main.js
@@ -159,15 +159,6 @@ jQuery(document).ready(function($) {
 		console.log("Browser doesn't support geolocation!");
 	}
 
-
-	// Get current path and find target link
-	var path = window.location.pathname.split("/").pop();
-
-	// Account for home page with empty path
-	if (path == '') {
-		path = 'index.html';
-	}
-
 	var target = $('#navbarSupportedContent ul li a[href="' + path + '"]');
 	// Add active class to target link
 	target.parent().addClass('active');
