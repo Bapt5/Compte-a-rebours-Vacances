337d46f85a6141a93c24a5a03692b150bec45963
Flavien Mathieu <72614724+Mflavien01@users.noreply.github.com>
2022-03-28 11:15:34 +0200

    Update main.js

--- main.js
+++ main.js
@@ -103,17 +103,6 @@ function test() {
 		});
 	});
 }
-$('#submit').on("click", function() {
-	zone = $('#zone-select').val();
-	vacDay = $('#dayVac-select').val();
-	heurVac = $('#time-Vac').val();
-	localStorage.setItem('zone', zone);
-	localStorage.setItem('vacDay', vacDay);
-	localStorage.setItem('heurVac', heurVac);
-	dateVacance();
-	$("#myModal").css("display", "none");
-	$('#week').click();
-});
 $(window).on('resize', function() {
 	setTimeout(function() { test(); }, 500);
 });
@@ -128,6 +117,17 @@ $(".close").on("click", function() {
 $(window).on("click", function(event) {
 	console.log(event.target.id);
 	if (event.target.id == "myModal") {
+		$("#myModal").css("display", "none");
+		$('#week').click();
+	}
+	if (event.target.id == "submit") {
+		zone = $('#zone-select').val();
+		vacDay = $('#dayVac-select').val();
+		heurVac = $('#time-Vac').val();
+		localStorage.setItem('zone', zone);
+		localStorage.setItem('vacDay', vacDay);
+		localStorage.setItem('heurVac', heurVac);
+		dateVacance();
 		$("#myModal").css("display", "none");
 		$('#week').click();
 	}
